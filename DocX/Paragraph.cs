98637e65ec0d11fbd5864e333e76fa8cca011c60
MadBoy_cp <SND\MadBoy_cp>
2012-06-06 23:51:18 +0000

    Cleanup in code: Removed unused directives from all DocX files

--- DocX/Paragraph.cs
+++ DocX/Paragraph.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Linq;
 using System.Text.RegularExpressions;
 using System.Security.Principal;
-using System.Collections;
 using System.IO.Packaging;
 using System.IO;
 using System.Drawing;
--- DocX/Table.cs
+++ DocX/Table.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Xml.Linq;
-using Novacode;
 using System.IO.Packaging;
 using System.IO;
-using System.Reflection;
 using System.Drawing;
 using System.Globalization;
 
