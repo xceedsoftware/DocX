d35a29d918a982a960f98060a029953c120f768f
MadBoy_cp <SND\MadBoy_cp>
2014-01-22 22:55:40 +0000

    Added missing bookmark.cs, fixed CRLF endings

---
+++ DocX/Bookmark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Novacode
+{
+    public class Bookmark
+    {
+        public string Name { get; set; }
+        public Paragraph Paragraph { get; set; }
+    }
+}
