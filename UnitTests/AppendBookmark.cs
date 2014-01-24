80e4d759c3ade058cac604c2a90ad44cd97d16f7
MadBoy_cp <pklys@evotec.pl>
2014-01-24 19:36:37 +0000

    Missing unit tests

---
+++ UnitTests/AppendBookmark.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Novacode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AppendBookmark
+    {
+        [TestMethod]
+        public void Bookmark_should_be_appended()
+        {
+            using (var doc = DocX.Create(""))
+            {
+                var paragraph = doc.InsertParagraph("A paragraph");
+                paragraph.AppendBookmark("bookmark");
+                var bookmarks = paragraph.GetBookmarks();
+                Assert.AreEqual(1, bookmarks.Count());
+            }
+        }
+
+        [TestMethod]
+        public void Bookmark_should_be_named_correctly()
+        {
+            using (var doc = DocX.Create(""))
+            {
+                var paragraph = doc.InsertParagraph("A paragraph");
+                paragraph.AppendBookmark("bookmark");
+                var bookmarks = paragraph.GetBookmarks();
+                Assert.AreEqual("bookmark", bookmarks.First().Name);
+            }
+        }
+
+        [TestMethod]
+        public void Bookmark_should_reference_paragraph()
+        {
+            using (var doc = DocX.Create(""))
+            {
+                var paragraph = doc.InsertParagraph("A paragraph");
+                paragraph.AppendBookmark("bookmark");
+                var bookmarks = paragraph.GetBookmarks();
+                Assert.AreEqual(paragraph, bookmarks.First().Paragraph);
+            }
+        }
+
+    }
+}
---
+++ UnitTests/InsertAtBookmark.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Novacode;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class InsertAtBookmark
+    {
+        [TestMethod]
+        public void Inserting_at_bookmark_should_add_text_in_paragraph()
+        {
+            using (var document = DocX.Create(""))
+            {
+                document.InsertParagraph("Hello ");
+                document.InsertBookmark("bookmark1");
+                document.InsertParagraph("!");
+
+                document.InsertAtBookmark("world", "bookmark1");
+
+                Assert.AreEqual("Hello world!", document.Text);
+
+            }
+        }
+
+        [TestMethod]
+        public void Inserting_at_bookmark_should_add_text_in_header()
+        {
+            using (var document = DocX.Create(""))
+            {
+                document.AddHeaders();
+                var header = document.Headers.even;
+                header.InsertParagraph("Hello ");
+                header.InsertBookmark("bookmark1");
+                header.InsertParagraph("!");
+
+                document.InsertAtBookmark("world", "bookmark1");
+
+                Assert.AreEqual("Hello world!", String.Join("", header.Paragraphs.Select(x=>x.Text)));
+
+            }
+        }
+
+        [TestMethod]
+        public void Inserting_at_bookmark_should_add_text_in_footer()
+        {
+            using (var document = DocX.Create(""))
+            {
+                document.AddHeaders();
+                var footer = document.Headers.even;
+                footer.InsertParagraph("Hello ");
+                footer.InsertBookmark("bookmark1");
+                footer.InsertParagraph("!");
+
+                document.InsertAtBookmark("world", "bookmark1");
+
+                Assert.AreEqual("Hello world!", String.Join("", footer.Paragraphs.Select(x => x.Text)));
+
+            }
+        }
+    }
+}
