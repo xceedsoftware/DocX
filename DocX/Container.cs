8794300fd3fcd26b02a6ad5e64fb0da538afd553
MadBoy_cp <pklys@evotec.pl>
2014-01-29 20:00:08 +0000

    Fixed parameter check newValue for Replace to be only checked for null. Having an empty string is allowed as replacement value.

--- DocX/Container.cs
+++ DocX/Container.cs
@@ -400,11 +400,10 @@ namespace Novacode
 
         public virtual void ReplaceText(string oldValue, string newValue, bool trackChanges = false, RegexOptions options = RegexOptions.None, Formatting newFormatting = null, Formatting matchFormatting = null, MatchFormattingOptions fo = MatchFormattingOptions.SubsetMatch)
         {
-            // PATCH BY HeDo - Arguments check!
             if (oldValue == null || oldValue.Length == 0)
                 throw new ArgumentException("oldValue cannot be null or empty", "oldValue");
 
-            if (newValue == null || newValue.Length == 0)
+            if (newValue == null)
                 throw new ArgumentException("newValue cannot be null or empty", "newValue");
             // ReplaceText in Headers of the document.
             Headers headers = Document.Headers;
