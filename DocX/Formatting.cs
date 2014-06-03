9ef3b466b852dd5c0c216936c5651256c8a075e7
MadBoy_cp <SND\MadBoy_cp>
2014-06-03 10:01:59 +0000

    Fixes two issues with ingesting a .docx file. First, a tab exists as a child of a tabs element, not an r element (fix in HelperFunctions.cs at line 146). Second, an rFonts element may not have a cs attribute, but instead may have an ascii, eastAsia or hAnsi attribute (fix in Formatting.cs at line 101).
    Patch provided by counsellorben

--- DocX/Formatting.cs
+++ DocX/Formatting.cs
@@ -74,12 +74,36 @@ namespace Novacode
             {
                 switch (option.Name.LocalName)
                 {
-                    case "lang": formatting.Language = new CultureInfo(option.GetAttribute(XName.Get("val", DocX.w.NamespaceName), null) ?? option.GetAttribute(XName.Get("eastAsia", DocX.w.NamespaceName), null) ?? option.GetAttribute(XName.Get("bidi", DocX.w.NamespaceName))); break;
-                    case "spacing": formatting.Spacing = Double.Parse(option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 20.0; break;
-                    case "position": formatting.Position = Int32.Parse(option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 2; break;
-                    case "kern": formatting.Position = Int32.Parse(option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 2; break;
-                    case "w": formatting.PercentageScale = Int32.Parse(option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))); break;
-                    case "rFonts": formatting.FontFamily = new FontFamily(option.GetAttribute(XName.Get("cs", DocX.w.NamespaceName))); break;
+                    case "lang": 
+                        formatting.Language = new CultureInfo(
+                            option.GetAttribute(XName.Get("val", DocX.w.NamespaceName), null) ?? 
+                            option.GetAttribute(XName.Get("eastAsia", DocX.w.NamespaceName), null) ?? 
+                            option.GetAttribute(XName.Get("bidi", DocX.w.NamespaceName))); 
+                        break;
+                    case "spacing": 
+                        formatting.Spacing = Double.Parse(
+                            option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 20.0; 
+                        break;
+                    case "position": 
+                        formatting.Position = Int32.Parse(
+                            option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 2; 
+                        break;
+                    case "kern": 
+                        formatting.Position = Int32.Parse(
+                            option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))) / 2; 
+                        break;
+                    case "w": 
+                        formatting.PercentageScale = Int32.Parse(
+                            option.GetAttribute(XName.Get("val", DocX.w.NamespaceName))); 
+                        break;
+                    case "rFonts": 
+                        formatting.FontFamily = 
+                            new FontFamily(
+                                option.GetAttribute(XName.Get("cs", DocX.w.NamespaceName), null) ??
+                                option.GetAttribute(XName.Get("ascii", DocX.w.NamespaceName), null) ??
+                                option.GetAttribute(XName.Get("hAnsi", DocX.w.NamespaceName), null) ??
+                                option.GetAttribute(XName.Get("eastAsia", DocX.w.NamespaceName))); 
+                        break;
                     case "vanish": formatting.hidden = true; break;
                     case "b": formatting.Bold = true; break;
                     case "i": formatting.Italic = true; break;
