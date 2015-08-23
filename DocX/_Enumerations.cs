b5c6005cd68824eb3c83e9c74e1bc8223781b6a3
MadBoy_cp <pklys@evotec.pl>
2015-08-23 09:39:23 +0000

    Patch provided by SebastianDotNet
    Added numbers in UnderlineStyle to the original word automation model enum numbers(WdUnderline).

--- DocX/_Enumerations.cs
+++ DocX/_Enumerations.cs
@@ -188,24 +188,24 @@ namespace Novacode
 
   public enum UnderlineStyle
   {
-    none,
-    singleLine,
-    doubleLine,
-    thick,
-    dotted,
-    dottedHeavy,
-    dash,
-    dashedHeavy,
-    dashLong,
-    dashLongHeavy,
-    dotDash,
-    dashDotHeavy,
-    dotDotDash,
-    dashDotDotHeavy,
-    wave,
-    wavyHeavy,
-    wavyDouble,
-    words
+      none = 0,
+      singleLine = 1,
+      words = 2,
+      doubleLine = 3,
+      dotted = 4,
+      thick = 6,
+      dash = 7,
+      dotDash = 9,
+      dotDotDash = 10,
+      wave = 11,
+      dottedHeavy = 20,
+      dashedHeavy = 23,
+      dashDotHeavy = 25,
+      dashDotDotHeavy = 26,
+      dashLongHeavy = 27,
+      dashLong = 39,
+      wavyDouble = 43,
+      wavyHeavy = 55,
   };
 
   public enum StrikeThrough
