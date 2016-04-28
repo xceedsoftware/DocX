57ba133ef4018b7107da4a0656ff051785b0f1de
Gerd Van Zegbroeck <gerd@vanzegbroeck.net>
2016-04-28 19:59:17 +0200

    Added DocX.iOS + iOS Test app

---
+++ DocX.iOS/System/Drawing/FontFamily.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace System.Drawing
+{
+	//	FontFamily
+
+	public class FontFamily : MarshalByRefObject
+	{
+		//	properties
+
+		public string Name { get; private set; }
+
+		//	constructor
+
+		public FontFamily (string name)
+		{
+			this.Name = name;
+		}
+	}
+}
---
+++ DocX.iOS/System/IO/Packaging/TargetMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO.Packaging
+{
+    public enum TargetMode
+    {
+        Internal,
+        External
+    }
+}
