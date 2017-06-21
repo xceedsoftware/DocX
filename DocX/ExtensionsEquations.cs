7cf3480ac7a715c643b2cdf012fc386c39d4538f
SheepRock <dcr110@gmail.com>
2017-06-21 13:04:22 -0300

    Added equation creation extensions
    Added file Equation.cs with Equation class, which allows an easy way to create complex equation.
    Added file ExtensionsEquations.cs which contains an extension that allow the insertion of Equation paragraphs.

---
+++ DocX/ExtensionsEquations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Novacode;
+using System.Xml.Linq;
+
+namespace OMath
+{
+    public static class ExtensionsEquations
+    {
+        public static Novacode.Paragraph InsertEquation(this DocX doc, Equation equation)
+        {
+            Paragraph eqParagraph = doc.InsertEquation("");
+            XElement xml = eqParagraph.Xml;
+            XNamespace mathNamespace = "http://schemas.openxmlformats.org/officeDocument/2006/math";
+            XElement omath = xml.Descendants(mathNamespace + "oMathPara").First();
+            omath.Elements().Remove();
+            omath.Add(equation.Xml);
+
+            return eqParagraph;
+        }
+    }
+}
